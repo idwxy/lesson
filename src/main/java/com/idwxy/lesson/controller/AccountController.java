94d19bad2c699665ad7945add2fb4901fc5a86b8
idwxy <idwxy@live.com>
2019-09-08 00:17:28 +0800

    添加 AccountController 注释

--- src/main/java/com/idwxy/lesson/controller/AccountController.java
+++ src/main/java/com/idwxy/lesson/controller/AccountController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 通过上传 Account 添加用户来熟悉 Jackson 的使用
 @RestController
 @RequestMapping("/account")
 public class AccountController {
